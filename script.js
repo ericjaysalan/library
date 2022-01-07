46e1ad3cd664b3712f2f710ba67f9b19a82df470
Eric Jay Salan <ericjaysalan@yahoo.com>
2022-01-07 19:46:18 +0800

    User can now create book objects

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const newBook = document.getElementById('new-book-btn');
 const overlay = document.getElementById('overlay');
 const modal = document.getElementById('form-modal');
+const form = modal.querySelector('#form-body form');
+const formTitle = document.querySelector('form #title');
+const formAuthor = document.getElementById('author');
+const formPages = document.getElementById('pages');
+const formRadio = document.getElementById('radio');
 const closeButton = document.getElementById('close-form');
 
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
-
-function addBookToLibrary(name, author, pages, read) {
-  // do stuff here
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function closeForm() {
@@ -23,6 +29,24 @@ function openForm() {
   overlay.classList.add('active');
 }
 
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
+
+function getInput(e) {
+  e.preventDefault();
+
+  const title = formTitle.value;
+  const author = formAuthor.value;
+  const pages = formPages.value;
+  let read = radio.firstElementChild.firstElementChild.checked ? true : false;
+
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+}
+
 newBook.addEventListener('click', openForm);
 overlay.addEventListener('click', closeForm);
 closeButton.addEventListener('click', closeForm);
+form.addEventListener('submit', getInput);
